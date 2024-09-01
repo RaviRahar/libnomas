3f55162f73e486acd54d4355f9026a58524aabf6
RaviRahar <ravirahar33@gmail.com>
2024-09-01 18:57:16 +0530

    fix: update example.c with correct example code
    Signed-off-by: RaviRahar <ravirahar33@gmail.com>

--- example.c
+++ example.c
@@ -3,10 +3,27 @@
 #include <stdio.h>
 #include <unistd.h>
 
-gboolean
-timeout_callback (gpointer user_data)
+typedef struct
 {
-    GMainLoop *loop = (GMainLoop *)user_data;
+    GMainLoop *loop;
+    NManager *manager;
+
+} callback_data;
+
+gboolean
+timeout_callback (gpointer user_data_pointer)
+{
+    callback_data *user_data = (callback_data *)user_data_pointer;
+    GMainLoop *loop = (GMainLoop *)user_data->loop;
+    n_manager_stop_listener (user_data->manager);
+    n_manager_save_state (user_data->manager);
+
+    /* const char *genericfile = "generic_history"; */
+    /* n_manager_save_to_file (manager, genericfile, GENERIC, TRUE); */
+    /* const char *dunstfile = "dunst_history"; */
+    /* n_manager_save_to_file (manager, dunstfile, DUNST, TRUE); */
+
+    g_object_unref (user_data->manager);
     g_main_loop_quit (loop);
     return G_SOURCE_REMOVE; // Remove the source after it is triggered
 }
@@ -26,37 +43,28 @@ main (gint argc, gchar *argv[])
             return 1;
         }
     const gchar *history_file = "notification_history";
-     NManager *manager = n_manager_new (); 
-     n_manager_set_history_file (manager, history_file, DUNST); 
+    NManager *manager = n_manager_new ();
+    n_manager_set_history_file (manager, history_file, DUNST);
+
+    /* n_manager_load_state (manager); */
+
+    /* const char *dunstfile = "dunst_history"; */
+    /* n_manager_load_from_file(manager, dunstfile, DUNST, TRUE); */
+    /* const char *genericfile = "generic_history"; */
+    /* n_manager_load_from_file (manager, genericfile, GENERIC, TRUE); */
 
     if (n_manager_start_listener (manager) < 0)
         {
             printf ("failed to start listener\n");
         }
 
-    g_timeout_add_seconds (20, timeout_callback, NULL);
     // Run the main loop
+    callback_data user_data = { loop, manager };
+    g_timeout_add_seconds (5, timeout_callback, &user_data);
     g_main_loop_run (loop);
 
     // Clean up
-    /* g_main_loop_unref (loop); */
+    g_main_loop_unref (loop);
 
-    /* n_manager_save_state (manager); */
-    /* n_manager_stop_listener (manager); */
-
-    /* n_manager_load_state (manager); */
-    /* n_manager_save_state (manager); */
-
-    /* const char *genericfile = "generic_history"; */
-    /* n_manager_save_to_file (manager, genericfile, GENERIC, TRUE); */
-    /* const char *dunstfile = "dunst_history"; */
-    /* n_manager_save_to_file (manager, dunstfile, DUNST, TRUE); */
-
-    /* const char *dunstfile = "dunst_history"; */
-    /* n_manager_load_from_file(manager, dunstfile, DUNST, TRUE); */
-    /* const char *genericfile = "generic_history"; */
-    /* n_manager_save_to_file (manager, genericfile, GENERIC, TRUE); */
-
-    /* g_object_unref (manager); */
     return EXIT_SUCCESS;
 }
